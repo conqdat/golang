040fd4a228b7f3401334911632b97c328b332bf4
DatTran <trancongdat.datct@gmail.com>
2023-07-22 12:47:18 +0700

    add code

--- hello.go
+++ hello.go
@@ -1,8 +1,15 @@
 package main
 
-import ("fmt")
+import "fmt"
 
 func main() {
-  num := 10
-  fmt.Println(num)
+	var s string = "Hello Dat"
+
+	var a, b int = 2, 3
+
+	const e int = 10
+
+	var d int
+	var h float32 = float32(a)
+	fmt.Println(s, a, b, d, e, h)
 }
