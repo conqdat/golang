b91c781e30bac10af1fbbd8f6438b2661993dff4
DatTran <trancongdat.datct@gmail.com>
2023-07-24 11:11:07 +0700

    add code for function

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Learn golang with love
 
-# 5. ****Slices****
+# 6. ****Function****
+
+### **Variadic Functions**
+
+```jsx
+func add(args ...int) int {
+  total := 0
+  for _, v := range args {
+    total += v
+  }
+  return total
+}
+func main() {
+  fmt.Println(add(1,2,3))
+}
+```
+
+### **Closure**
+
+```jsx
+func main() {
+  add := func(x, y int) int {
+    return x + y
+  }
+  fmt.Println(add(1,1))
+}
+```
+
+### **Returning Multiple Values**
+
+```jsx
+func f() (int, int) {
+  return 5, 6
+}
+
+func main() {
+  x, y := f()
+}
+```
--- hello.go
+++ hello.go
@@ -1,18 +1,21 @@
 package main
 
 import "fmt"
-import "reflect"
 
 func main() {
-	s := []int{2, 2, 3}
-	sliceString := []string{"1", "2", "3"}
+	// Anonymous functions
+	goMod := func(a, b int) int {
+		return a % b
+	}
 
-	subSlice := s[:]      // get all items in slice s
-	subSliceTwo := s[0:2] // from index = 1, get 2 items
+	fmt.Println(goMod(10, 34))
+}
 
-	s = append(s, 4) // add item to slice
+func sum(a, b int) int {
+	return a + b
+}
 
-	d := append(s, subSlice...) // append slice to slice
-
-	fmt.Println(s, reflect.TypeOf(s), sliceString, subSlice, subSliceTwo, d)
+// function return multiple value
+func logger(a, b int) (int, int) {
+	return a, b
 }
