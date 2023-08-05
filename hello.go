f09a38020191552d7cd9e41544b0a2e5699db1fc
DatTran <trancongdat.datct@gmail.com>
2023-08-05 15:27:24 +0700

    add

--- hello.go
+++ hello.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var arr [3]int           // array with default value [0, 0, 0]
@@ -19,4 +21,51 @@ func main() {
 	}
 
 	fmt.Println(arr, arr2, arrBool, length)
+
+	fmt.Println("********************")
+
+	var myBooks [5]string
+	myBooks[1] = "Book 1"
+	myBooks[4] = "Book 5"
+
+	var myNum []int8
+	myNum = append(myNum, 0)
+	myNum = append(myNum, 1)
+	myNum = append(myNum, 2)
+	myNum = append(myNum, 3)
+	myNum = append(myNum, 4)
+
+	mySlice := make([]int8, 8)
+	mySlice[0] = 5
+	mySlice[1] = 6
+	mySlice[2] = 7
+	mySlice[3] = 8
+	mySlice[4] = 9
+
+	fmt.Println(myBooks, myNum, cap(myNum), len(myNum), mySlice)
+
+	for index, value := range myNum {
+		fmt.Println(" i: ", index, " value: ", value)
+	}
+
+	fmt.Println("==========")
+	name := "Cong Dat"
+
+	for index, value := range name {
+		fmt.Println(index, value)
+	}
+
+	hiDat := sayHi("Cong Dat")
+	fmt.Println(hiDat)
+
+	fmt.Println("--------------")
+
+	myMap := make(map[string]string)
+
+	myMap["name"] = "Dat"
+	myMap["age"] = "20"
+	myMap["add"] = "HCM"
+
+	fmt.Println("my Map: ", myMap, len(myMap))
+
 }
---
+++ helper.go
@@ -0,0 +1,5 @@
+package main
+
+func sayHi(name string) string {
+	return "Hi" + name
+}
