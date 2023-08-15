762cffe5b78f2f53a883a05b9584f6380dc9edca
DatTran <trancongdat.datct@gmail.com>
2023-08-15 23:55:09 +0700

    add ex

--- main.go
+++ main.go
@@ -14,11 +14,34 @@ func (r rect) per() int {
 	return int(2*r.width + 2*r.height)
 }
 
+type Student struct {
+	name string
+	age  int8
+}
+
+type StudentRepo struct {
+	students []Student
+}
+
+func (studentRepo *StudentRepo) add(student Student) []Student {
+	studentRepo.students = append(studentRepo.students, student)
+	return studentRepo.students
+}
+
 func main() {
 	a := rect{width: 10, height: 10}
 
 	fmt.Println(a.area())
 	fmt.Println(a.per())
+	fmt.Println("===============")
+	congdat := Student{name: "Cong Dat", age: 20}
+
+	c := StudentRepo{}
+	c.add(congdat)
+	c.add(congdat)
+
+	fmt.Println(c)
+
 }
 
 func isTriangle(a, b, c int) bool {
