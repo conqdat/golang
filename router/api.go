6d3e4ee9dd783b2776711b710aabb192dcd9282e
DatTran <trancongdat.datct@gmail.com>
2023-11-05 02:28:20 +0700

    :rotating_light: add code for middleware

--- router/api.go
+++ router/api.go
@@ -1,17 +1,17 @@
 package router
 
 import (
-	"myapp/handler"
-
 	"github.com/labstack/echo/v4"
+	"myapp/handler"
+	mymiddleware "myapp/middleware"
 )
 
 type API struct {
-	Echo *echo.Echo
+	Echo       *echo.Echo
 	Userhandle handler.UserHandle
 }
 
 func (api *API) SetUpRouter() {
-	api.Echo.POST("/sign-in", api.Userhandle.HandleSignIn)
+	api.Echo.POST("/sign-in", api.Userhandle.HandleSignIn, mymiddleware.IsAdmin())
 	api.Echo.POST("/sign-up", api.Userhandle.HandleSignUp)
 }
