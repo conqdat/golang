e96a304f999d2e4196ef832797607df774faa2c6
DatTran <trancongdat.datct@gmail.com>
2024-04-11 22:46:32 +0700

    remove

--- banana/err_user.go
+++
@@ -1,9 +0,0 @@
-package banana
-
-import "errors"
-
-var (
-	UserConflict = errors.New("user already exist")
-	SignUpFailed = errors.New("sign up Failed ")
-	UserNotFound = errors.New("user not found")
-)
--- model/req/req_signup.go
+++
@@ -1,7 +0,0 @@
-package req
-
-type ReqSignUp struct {
-	FullName string `json:"full_name" validate:"required"`
-	Email    string `json:"email" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
